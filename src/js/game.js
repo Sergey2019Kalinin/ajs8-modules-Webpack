8821df3b305a62598b1d37ee5ccb2fcae9ff6c22
Sergey2019Kalinin <togiveknowledge@gmail.com>
2022-06-23 19:00:46 +0500

    first commit

---
+++ src/js/game.js
@@ -0,0 +1,21 @@
+class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+class GameSavingData {
+}
+
+function readGameSaving() {
+}
+
+function writeGameSaving() {
+}
+
+import class Character from './src/js/domain.js'
+
+export default class Character
+
+export {readGameSaving, writeGameSaving}
+
